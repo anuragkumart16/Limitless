54a3d9cae19012e5ac597ab1b4603e2418412e6a
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-04-22 12:59:46 +0530

    deleted frontend for fixing

--- Frontend/src/Components/Atom/SecondaryHeading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SecondaryHeading({children,textAlign='left',margin='0rem'}) {
-  return (
-    <h2 style={{fontSize:'1.5rem',color:'#ffffff',fontWeight:'500',textAlign:textAlign,margin:margin}}>{children}</h2>
-  )
-}
-
-export default SecondaryHeading
--- Frontend/src/Components/Atom/Span.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Span({children,color="#ffffff",fontSize="1rem"}) {
-  return (
-    <span style={{color:color,fontSize:fontSize}}>{children}</span>
-  )
-}
-
-export default Span
